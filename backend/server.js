506e44eb2b5499a96777730d37d3ebb48fe2c445
melbch <hanssonmelina@hotmail.com>
2026-04-07 20:44:47 +0200

    Fixed faulty path

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,15 @@ app.get('/', (req, res) => {
     res.send('API is running');
 })
 
-app.options('*', cors());
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', 'https://coworking-frontend-6vjn.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
