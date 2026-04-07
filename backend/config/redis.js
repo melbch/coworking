c0c6396257d848dbc499b2cc06596f34b84c933d
melbch <hanssonmelina@hotmail.com>
2026-04-07 18:27:12 +0200

    added check to see that Redis connects properly

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -8,6 +8,7 @@ redisClient.on('error', (err) => console.error('Redis error', err));
 
 (async () => {
     await redisClient.connect();
+    console.log('Redis connected');
 })();
 
 module.exports = redisClient;
